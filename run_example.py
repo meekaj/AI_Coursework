2fcfae9e2ca38a3e96a375fe1bec3e3aac40aedd
Meeka <mihikajain@icloud.com>
2025-11-17 17:36:14 +0000

    task 1 complete! added colour to maze

--- run_example.py
+++ run_example.py
@@ -5,32 +5,54 @@ from astar import astar_search
 from agent import follow_path
 import numpy as np
 
-def print_grid(desc, path=None):
-    #want to see path made clearly...
+def print_grid(desc, path=None, agent_pos=None):
+    RESET  = "\033[0m"
+    YELLOW = "\033[93m"
+    RED    = "\033[91m"
+    GREEN  = "\033[92m"
+
     path_set = set(path) if path is not None else set()
 
     for r in range(desc.shape[0]):
         row_symbols = []
         for c in range(desc.shape[1]):
             symbol = desc[r, c]
-            #mark path cells * but keep I and G visible
-            if (r, c) in path_set and symbol not in ["I", "G"]:
-                symbol = "*"
-            #width 2 for alignment
-            row_symbols.append(f"{symbol:2}")
+
+            if agent_pos is not None and (r, c) == agent_pos:
+                vis = "A"
+            elif (r, c) in path_set and symbol not in ["I", "G"]:
+                vis = "*"
+            else:
+                vis = symbol 
+
+            cell = f"{vis:2}"
+
+            #colour
+            if agent_pos is not None and (r, c) == agent_pos:
+                cell = f"{RED}{cell}{RESET}"
+            elif (r, c) in path_set and symbol not in ["I", "G"]:
+                cell = f"{YELLOW}{cell}{RESET}"
+            elif symbol == "G":
+                cell = f"{GREEN}{cell}{RESET}"
+
+            row_symbols.append(cell)
 
         print(" ".join(row_symbols))
     print()
 
+
 if __name__ == "__main__":
     #1. random maze generator
-    desc, portals = generate_flmp(size=11, wall_prob=0.2, hole_prob=0.15,
-                                            n_portals=2, seed=123)
+    desc, portals = generate_flmp(size=12, wall_prob=0.2, hole_prob=0.15,
+                                            n_portals=2)
     env = FLMPEnv(desc=desc, portals=portals, p=1,render_mode="ansi") #change p accordingly...
+
+    obs, _ = env.reset()
+    agent_r, agent_c = env.to_row_col(obs)
 
     #2. show maze grid
     print("Generated Maze:")
-    print_grid(desc)
+    print_grid(desc, agent_pos=(agent_r, agent_c))
     #3. show portals dictionary
     print("Portals:", portals, "\n") 
 
@@ -39,15 +61,14 @@ if __name__ == "__main__":
     print("Path found by A*, shown as a list of (row, col) cells:")
     print (path, "\n")
 
+    
+
     if path is not None:
         print("Maze with path marked with *")
-        print_grid(desc, path = path)
-    else:
-        print ("A* could not find path")
+        print_grid(desc, path=path, agent_pos=(agent_r, agent_c))
 
     #env = FLMPEnv(desc=desc, portals=portals, render_mode="ansi")
-    obs, _ = env.reset()
-
+    
     #print("inital env")
     #print(env.render())
     
